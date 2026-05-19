51779f8dc04eba857ea16ba3c9267e4e3834af0b
9Phoenix1 <3276923826@qq.com>
2026-05-19 22:36:32 +0800

    Delete src directory

--- src/main/resources/db/alter_avatar_field.sql
+++
@@ -1,2 +0,0 @@
--- 修改 avatar 字段类型，支持存储 Base64 图片数据
-ALTER TABLE users MODIFY COLUMN avatar TEXT COMMENT '头像URL或Base64数据';
--- src/main/resources/db/alter_post_images.sql
+++
@@ -1,2 +0,0 @@
--- 修改 post_images 表的 image_url 字段为 TEXT 类型，支持存储 Base64 图片
-ALTER TABLE post_images MODIFY COLUMN image_url TEXT NOT NULL COMMENT '图片URL或Base64数据';
